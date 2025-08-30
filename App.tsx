6cb8f6ac7498b3f072bb5d2a05b042e4367ffdd0
yamchapnik-hub <yam.chapnik@gmail.com>
2025-08-30 19:57:40 +0200

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { CartProvider } from "@/hooks/use-cart";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/Home";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </CartProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
